539e8f24dba0bef5325dbdd66c1aa9748716c093
btl <btl@claptrap>
2014-07-24 14:47:08 +0200

    modif sortie function

--- lib/puppet/parser/functions/rabbitmq_instances.rb
+++ lib/puppet/parser/functions/rabbitmq_instances.rb
@@ -1,17 +1,23 @@
 module Puppet::Parser::Functions
     newfunction(:rabbitmq_instances,:type => :rvalue) do |args|
-        tbl=[]
+        tbl=""
         listip = args[0]
         nameinstance=args[1]
         vtype = args[2]
+        cpt=0
         if vtype == "full" 
             listip['ip'].each do |element|
-                tbl << "#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+                if cpt == 0
+                  tbl=tbl+"#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+                else
+                  tbl=tbl+", #{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+                end
+            cpt=cpt++
             end
         else
             listip['ip'].each do |element|
                 if element == lookupvar('ipaddress') 
-                    tbl << "#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+                    tbl = "#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
                 end
             end
         end
