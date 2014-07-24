7c43135134f5f9d3fbb72dc9f3e5f45a3fd0f315
Hans Moulron <hans.moulron@francetv.fr>
2014-07-24 15:59:33 +0100

    opti fonction rabbitmq_instances

--- lib/puppet/parser/functions/rabbitmq_instances.rb
+++ lib/puppet/parser/functions/rabbitmq_instances.rb
@@ -1,29 +1,24 @@
 module Puppet::Parser::Functions
   newfunction(:rabbitmq_instances,:type => :rvalue) do |args|
-    tbl=""
-    listip = args[0]
-    nameinstance=args[1]
-    vtype = args[2]
-    cpt=0
+    tbl          = ''
+    listip       = args[0]
+    nameinstance = args[1]
+    vtype        = args[2]
+    node         = []
     Puppet.debug("VALUE = #{nameinstance} - #{vtype} - #{listip[0]} ")
-    if vtype == "full" 
+    if vtype == "full"
       listip['ip'].each do |element|
-        if cpt == 0
-          tbl=tbl+"#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
-        else
-          tbl=tbl+", #{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
-        end
-        Puppet.debug("VALUECP = #{cpt}")
-        cpt=cpt+1
+        node.push("#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}")
       end
+      tbl = node.join(', ')
      else
       listip['ip'].each do |element|
-        if element == lookupvar('ipaddress') 
+        if element == lookupvar('ipaddress')
           tbl = "#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
         end
       end
     end
     Puppet.debug("VALUE = #{tbl}")
-    tbl
+    return tbl
   end
 end
