1a8906e01a7f9b97644b0117cad0145c3d860440
btl <btl@claptrap>
2014-07-24 13:25:02 +0200

    Modif function list

--- lib/puppet/parser/functions/rabbitmq_instances.rb
+++ lib/puppet/parser/functions/rabbitmq_instances.rb
@@ -1,14 +1,20 @@
 module Puppet::Parser::Functions
     newfunction(:rabbitmq_instances,:type => :rvalue) do |args|
-
         tbl=[]
         listip = args[0]
         nameinstance=args[1]
         vtype = args[2]
-        cpt=0
-        listip['ip'].each do |element|
-                tbl << "#{element[1]}"
+        if vtype == "full" 
+            listip['ip'].each do |element|
+                tbl << " #{nameinstance}+"@"+#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+            end
+        else
+            listip['ip'].each do |element|
+                if element == lookupvar('ipaddress') 
+                    tbl << " #{nameinstance}+"@"+#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+                end
+            end
         end
-    tbl
+        tbl
     end
 end
