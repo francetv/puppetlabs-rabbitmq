78d228ed5f931b28965ee54670217ef37dc90ab0
btl <btl@claptrap>
2014-07-25 10:06:09 +0200

    Modif nom fonction

--- lib/puppet/parser/functions/rabbitmq_nodelist.rb
+++ lib/puppet/parser/functions/rabbitmq_nodelist.rb
@@ -1,5 +1,5 @@
 module Puppet::Parser::Functions
-  newfunction(:rabbitmq_instances,:type => :rvalue) do |args|
+  newfunction(:rabbitmq_nodelist,:type => :rvalue) do |args|
     tbl          = []
     listip       = args[0]
     nameinstance = args[1]
