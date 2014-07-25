d515dc8b9d197513c36f80e3ca6329c155cf6786
btl <btl@claptrap>
2014-07-25 09:52:07 +0200

    Modif sortie fonction

--- lib/puppet/parser/functions/rabbitmq_instances.rb
+++ lib/puppet/parser/functions/rabbitmq_instances.rb
@@ -1,20 +1,18 @@
 module Puppet::Parser::Functions
   newfunction(:rabbitmq_instances,:type => :rvalue) do |args|
-    tbl          = ''
+    tbl          = []
     listip       = args[0]
     nameinstance = args[1]
     vtype        = args[2]
-    node         = []
     Puppet.debug("VALUE = #{nameinstance} - #{vtype} - #{listip} ")
     if vtype == "full"
       listip['ip'].each do |element|
-        node.push("#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}")
+        tbl << "#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}"
       end
-      tbl = node.join(', ')
      else
       listip['ip'].each do |element|
         if element[1] == lookupvar('ipaddress')
-          tbl = "#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}"
+          tbl << "#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}"
         end
       end
     end
