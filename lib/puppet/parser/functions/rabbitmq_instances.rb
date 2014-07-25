1b682e7084dab2c1cb7867b45d9bd992ca65868b
Hans Moulron <hans.moulron@francetv.fr>
2014-07-25 08:27:04 +0200

    debug newfonction puppet

--- lib/puppet/parser/functions/rabbitmq_instances.rb
+++ lib/puppet/parser/functions/rabbitmq_instances.rb
@@ -5,16 +5,16 @@ module Puppet::Parser::Functions
     nameinstance = args[1]
     vtype        = args[2]
     node         = []
-    Puppet.debug("VALUE = #{nameinstance} - #{vtype} - #{listip[0]} ")
+    Puppet.debug("VALUE = #{nameinstance} - #{vtype} - #{listip} ")
     if vtype == "full"
       listip['ip'].each do |element|
-        node.push("#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}")
+        node.push("#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}")
       end
       tbl = node.join(', ')
      else
       listip['ip'].each do |element|
-        if element == lookupvar('ipaddress')
-          tbl = "#{nameinstance}"+"@"+"#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{listip[0]}"
+        if element[1] == lookupvar('ipaddress')
+          tbl = "#{lookupvar('project')}-#{lookupvar('ftven_env')}-#{lookupvar('role')}-#{element[0]}"
         end
       end
     end
